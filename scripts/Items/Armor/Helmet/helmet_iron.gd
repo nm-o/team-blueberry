ed3a15291e1042c029b57ad9cceb3d0ed5628243
Zamaya Toledo <zamy.canela@gmail.com>
2025-10-25 20:22:45 -0300

    helmet scripts and assets ok

--- scripts/Items/Armor/Helmet/helmet_iron.gd
+++ scripts/Items/Armor/Helmet/helmet_iron.gd
@@ -4,7 +4,7 @@ extends Helmet
 # Function that acivates once the game starts
 func _init() -> void:
 	description = "Helmet: keep yourself safe"
-	texture = "res://assets/OriginalAssets/item_placeholders/helmet_test.png"
+	texture = "res://assets/Items/Craftables/helmet_iron.png"
 	max_number = 3
 	name = "Iron Helmet"
 	armor_modifier = 0.4*0.3
--- scripts/Items/Armor/Helmet/helmet_legendary.gd
+++ scripts/Items/Armor/Helmet/helmet_legendary.gd
@@ -4,7 +4,7 @@ extends Helmet
 # Function that acivates once the game starts
 func _init() -> void:
 	description = "Helmet: keep yourself safe"
-	texture = "res://assets/OriginalAssets/item_placeholders/helmet_test.png"
+	texture = "res://assets/Items/Craftables/helmet_legendary.png"
 	max_number = 3
 	name = "Legendary Helmet"
 	armor_modifier = 0.7*0.3
