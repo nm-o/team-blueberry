d26efccf7a75210ae441e0b5d3173a8446f5bbec
Zamaya Toledo <zamy.canela@gmail.com>
2025-10-25 20:07:32 -0300

    axe scripts and assets ok

--- scripts/Items/Weapons/Axe/axe_iron.gd
+++ scripts/Items/Weapons/Axe/axe_iron.gd
@@ -4,7 +4,7 @@ extends Axe
 # Function that acivates once the game starts
 func _init() -> void:
 	description = "Axe: mutilate them"
-	texture = "res://assets/OriginalAssets/item_placeholders/sword_test.png"
+	texture = "res://assets/Items/Craftables/axe_iron.png"
 	max_number = 1
 	name = "Iron Axe"
 	base_damage = 10
--- scripts/Items/Weapons/Axe/axe_legendary.gd
+++ scripts/Items/Weapons/Axe/axe_legendary.gd
@@ -4,7 +4,7 @@ extends Axe
 # Function that acivates once the game starts
 func _init() -> void:
 	description = "Axe: mutilate them"
-	texture = "res://assets/OriginalAssets/item_placeholders/sword_test.png"
+	texture = "res://assets/Items/Craftables/axe_legendary.png"
 	max_number = 1
 	name = "Legendary Axe"
 	base_damage = 20
