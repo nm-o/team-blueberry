93017a028e0769f5e568dddac25ea966ee84518d
Zamaya Toledo <zamy.canela@gmail.com>
2025-10-25 22:28:22 -0300

    freeze potions scripts and assets ok

---
+++ scripts/Items/Potions/potion_freeze_1.gd
@@ -0,0 +1,11 @@
+class_name PotionFreeze1
+extends Potion
+
+# Function that acivates once the game starts
+func _init() -> void:
+	description = "Basic Freeze Potion"
+	texture = "res://assets/Items/Craftables/potion_frost_small.png"
+	max_number = 10
+	name = "Basic Freeze Potion"
+	time = 5
+	effect = Global.States.FROZEN
---
+++ scripts/Items/Potions/potion_freeze_2.gd
@@ -0,0 +1,11 @@
+class_name PotionFreeze2
+extends Potion
+
+# Function that acivates once the game starts
+func _init() -> void:
+	description = "Advanced Freeze Potion"
+	texture = "res://assets/Items/Craftables/potion_frost_big.png"
+	max_number = 10
+	name = "Advanced Freeze Potion"
+	time = 10
+	effect = Global.States.FROZEN
