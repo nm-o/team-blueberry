b6f115d67038c3f53a487aa56e49946d7f6c1421
Zamaya Toledo <zamy.canela@gmail.com>
2025-10-25 22:42:56 -0300

    all flowers scripts and assets ok

--- scripts/Items/Materials/flower_heal.gd
+++ scripts/Items/Materials/flower_heal.gd
@@ -4,6 +4,6 @@ extends Materials
 # Function that acivates once the game starts
 func _init() -> void:
 	description = "Healing flower:"
-	texture = "res://assets/OriginalAssets/item_placeholders/iron_test.png"
+	texture = "res://assets/Items/Materials/flower_heal.png"
 	max_number = 10
 	name = "Healing flower"
