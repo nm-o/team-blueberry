9c4c6e0292ac8d75e34ff3c1e6f6d03797953209
Zamaya Toledo <zamy.canela@gmail.com>
2025-10-28 21:03:28 -0300

    added all recipes for brewing station

--- scripts/crafting/brewing_station_control.gd
+++ scripts/crafting/brewing_station_control.gd
@@ -4,5 +4,20 @@ extends CraftingControl
 var accepted_player_roles = ["Alchemist"]
 
 func _ready() -> void:
-	recipes = {}
+	recipes = {
+		# Level 1
+		["Water", "Poisonous flower", ""]: PotionPoison1,
+		["Water", "", "Poisonous flower"]: PotionPoison1,
+		["Water", "Healing flower", ""]: PotionHeal1,
+		["Water", "", "Healing flower"]: PotionHeal1,
+		["Water", "Frost flower", ""]: PotionFreeze1,
+		["Water", "", "Frost flower"]: PotionFreeze1,
+		# Level 2
+		["Water", "Poisonous flower", "Magic Crystal"]: PotionPoison2,
+		["Water", "Magic Crystal", "Poisonous flower"]: PotionPoison2,
+		["Water", "Healing flower", "Magic Crystal"]: PotionHeal2,
+		["Water", "Magic Crystal",  "Healing flower"]: PotionHeal2,
+		["Water", "Frost flower", "Magic Crystal"]: PotionFreeze2,
+		["Water", "Magic Crystal", "Frost flower"]: PotionFreeze2,
+	}
 	super_ready()
