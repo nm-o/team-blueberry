2f1459a3ebddd4df5bacdb4fbd796abb21b81ac9
Your Name <you@example.com>
2025-12-09 00:49:14 -0300

    add: sistema recoleccion basico funcionando

---
+++ scripts/Items/Materials/ResourceNode.gd
@@ -0,0 +1,34 @@
+extends Node2D
+class_name ResourceNode
+
+@export var item_script: Script          # Script del Item/Potion/etc.
+@export var amount: int = 1             # Cuántas unidades da
+@export var respawn_time: float = 0.0   # 0 = no respawnea
+
+@onready var interaction_area: Area2D = $InteractionArea
+@onready var interaction_name_label: Label = $InteractionName
+
+var current_amount: int
+
+func _ready() -> void:
+	current_amount = amount
+	interaction_name_label.visible = false
+	# Para que entre en el sistema de interacción del Player
+	interaction_area.owner = self
+
+func interact():
+	if current_amount <= 0:
+		return
+	if not item_script:
+		return
+
+	var item: Item = item_script.new()
+
+	item.drop_id = Mouse.get_drop_id()
+
+	for i in current_amount:
+		Mouse.player.inventory.add_item(item)
+
+	current_amount = 0
+	# Desaparecer o cambiar sprite
+	queue_free()  # o cambiar a sprite de "agotado"
